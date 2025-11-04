35ddf8301ebd39e2d9b6a1c344d5317bc30c4b03
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-11-04 16:10:43 +0500

    ch2_L18_end Работа с union типами

--- ch2/test/index.ts
+++ ch2/test/index.ts
@@ -1,30 +1,8 @@
-const userData = {
-  isBirthdayData: true,
-  ageData: 40,
-  userNameData: "John",
-  messages: {
-    error: "Error"
-  }
+const message: string | number | boolean = 5;
+const messages: string[] | number[] = ['a','b']
+
+function printMsg(msg: string | number) {
+  console.log(msg);
 }
 
-function logBrtMsg({isBirthdayData, userNameData, ageData, messages: {error}}: {
-  isBirthdayData: boolean,
-  userNameData: string,
-  ageData: number,
-  messages: { error: string }
-}): string {
-  return ''
-}
-
-logBrtMsg(userData)
-
-const departments: string[] = ['dev', 'design', 'marketing']
-const nums: number[] = [3, 5, 6]
-
-const report = departments.filter((d: string) => d !== 'dev')
-    .map((d: string) => `${d} - done`)
-
-const userDataTuple: [boolean, number, ...string[]] = [true, 40, "John",'Alex','Ann'];
-
-const res = userDataTuple.map((t) => `${t} - data`)
-const [bthd, age, username] = userDataTuple;
+printMsg('dkdkddkdkdk')

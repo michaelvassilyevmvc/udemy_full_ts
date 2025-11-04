9cb51393d93ecde0288e981ad56c7853dd5f1b0a
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-11-04 13:08:28 +0500

    ch2_L9_end Установка типа для объекта

--- ch2/test/index.ts
+++ ch2/test/index.ts
@@ -1,7 +1,28 @@
-const isBirthday: boolean = true;
-const age: number = 40;
-const userName = 'John';
+const isBirthdayData: boolean = true;
+const ageData: number = 40;
+const userNameData: string = 'John';
 
-if (isBirthday) {
-  console.log(`Congrats ${userName}, age: ${age + 1}`);
+// function logBrtMsg(isBirthday: boolean, userName: string, age: number): void {
+//   if (isBirthday) {
+//     console.log(`Congrats ${userName.toUpperCase()}, age: ${age + 1}`);
+//   }
+// }
+
+
+const userData = '{"isBirthdayData":true, "ageData":40,"userNameData":"John"}';
+const userObj: {
+  isBirthdayData: boolean,
+  userNameData: string,
+  ageData: number
+} = JSON.parse(userData);
+
+const logBrtMsg = (
+    isBirthday: boolean,
+    userName: string,
+    age: number): void => {
+  if (isBirthday) {
+    console.log(`Congrats ${userName.toUpperCase()}, age: ${age + 1}`);
+  }
 }
+
+logBrtMsg(isBirthdayData, userNameData, ageData);

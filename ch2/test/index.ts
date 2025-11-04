b744158a7255596c6f57c6eb972f836099736c75
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-11-04 15:16:20 +0500

    ch2_L14_end Деструктуризация объекта

--- ch2/test/index.ts
+++ ch2/test/index.ts
@@ -1,9 +1,19 @@
-let id: symbol = Symbol('id')
-
-const data = {
-  [id]:1
+const userData = {
+  isBirthdayData: true,
+  ageData: 40,
+  userNameData: "John",
+  messages: {
+    error: "Error"
+  }
 }
 
-console.log(data[id])
+function logBrtMsg({isBirthdayData, userNameData, ageData, messages: {error}}: {
+  isBirthdayData: boolean,
+  userNameData: string,
+  ageData: number,
+  messages: { error: string }
+}): string {
+  return ''
+}
 
-const num1 = 1n;
+logBrtMsg(userData)

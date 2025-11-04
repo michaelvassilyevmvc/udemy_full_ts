eb1e593e6c1e250481c551a73f6bc60a44839f10
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-11-04 16:24:20 +0500

    ch2_L20_end Сужение union-типов за счет typeof, in и instanceof

--- ch2/test/index.ts
+++ ch2/test/index.ts
@@ -1,8 +1,45 @@
 const message: string | number | boolean = 5;
-const messages: string[] | number[] = ['a','b']
+const messages: string[] | number[] = ['a', 'b']
 
-function printMsg(msg: string | number) {
-  console.log(msg);
+// function printMsg(msg: string | number | boolean) {
+//   if (typeof msg === 'string' || typeof msg === 'number') {
+//     console.log(msg.toString());
+//   } else {
+//     console.log(msg);
+//   }
+// }
+
+function printMsg(msg: string[] | number | boolean) {
+  if (Array.isArray(msg)) {
+    msg.forEach((m) => console.log(m));
+  } else if (typeof msg === 'number') {
+    console.log(msg.toFixed(0));
+  } else {
+    console.log(msg);
+  }
 }
 
-printMsg('dkdkddkdkdk')
+const printReading = (a: number | string, b: number | boolean) => {
+  if (a === b) {
+    console.log(a, b);
+  }
+}
+const printReading2 = (a: number[] | string) => {
+  console.log(a.slice(0, 3))
+}
+
+function checkReadings(readings: { system: number } | { user: number }): void {
+  if ('system' in readings) {
+    console.log(readings.system);
+  } else {
+    console.log(readings.user);
+  }
+}
+
+function logValue(x: string | Date) {
+  if (x instanceof Date) {
+    console.log(x.getDate());
+  } else {
+    console.log(x.trim());
+  }
+}

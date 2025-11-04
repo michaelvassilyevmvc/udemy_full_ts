84ad7f62e6e3e5042daf5c2a9a798e901cee13f9
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-11-04 16:03:17 +0500

    ch2_L17_end Работа с кортежами и spread оператором(...)

--- ch2/test/index.ts
+++ ch2/test/index.ts
@@ -23,3 +23,8 @@ const nums: number[] = [3, 5, 6]
 
 const report = departments.filter((d: string) => d !== 'dev')
     .map((d: string) => `${d} - done`)
+
+const userDataTuple: [boolean, number, ...string[]] = [true, 40, "John",'Alex','Ann'];
+
+const res = userDataTuple.map((t) => `${t} - data`)
+const [bthd, age, username] = userDataTuple;

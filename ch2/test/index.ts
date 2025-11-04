e9f6d9ac3082db64df1670cfd11a47a065406746
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-11-04 13:17:50 +0500

    ch2_L10_self_end Указаны типы данных для переменных и параметров

--- ch2/test/index.ts
+++ ch2/test/index.ts
@@ -1,28 +1,18 @@
-const isBirthdayData: boolean = true;
-const ageData: number = 40;
-const userNameData: string = 'John';
+const currRate = "1.05";
 
-// function logBrtMsg(isBirthday: boolean, userName: string, age: number): void {
-//   if (isBirthday) {
-//     console.log(`Congrats ${userName.toUpperCase()}, age: ${age + 1}`);
-//   }
-// }
+const fetchCurr = (response:string):number => {
+  const data: number = JSON.parse(response);
+  return data;
+};
 
-
-const userData = '{"isBirthdayData":true, "ageData":40,"userNameData":"John"}';
-const userObj: {
-  isBirthdayData: boolean,
-  userNameData: string,
-  ageData: number
-} = JSON.parse(userData);
-
-const logBrtMsg = (
-    isBirthday: boolean,
-    userName: string,
-    age: number): void => {
-  if (isBirthday) {
-    console.log(`Congrats ${userName.toUpperCase()}, age: ${age + 1}`);
+function transferEurToUsd(available: boolean, amount: number, commission:number):void {
+  if (available) {
+    let res:number = fetchCurr(currRate) * amount * commission;
+    console.log(res);
+    // Или запись в элемент на странице вместо консоли
+  } else {
+    console.log("Сейчас обмен недоступен");
   }
 }
 
-logBrtMsg(isBirthdayData, userNameData, ageData);
+transferEurToUsd(true, 500, 1.05);

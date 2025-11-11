4847690840553ca13e57a0d1f5c5f6a239ceab4e
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-11-11 12:25:27 +0500

    ch3_L34_self_end Работа с enum и типизацией полей и функций

---
+++ ch3/test/self34.ts
@@ -0,0 +1,67 @@
+// Перечисление с названием TypesOfMedia, которое включает строчные типы video, audio
+enum TypesOfMedia {
+  video ="video",
+  audio=  "audio"
+}
+
+// Перечисление с названием FormatsOfMedia, которое включает строчные видео-форматы: .mp4, .mov, .mkv, .flv, .webM
+enum FormatsOfMedia {
+  mp4 ='.mp4',
+  mov = '.mov',
+  mkv = '.mkv',
+  flv = '.flv',
+  webM = '.webM',
+}
+
+// Описание интерфейса, в котором:
+// name - строка
+// type - один из перечисления выше
+// format = один из перечисления выше
+// subtitles - необязательное поле типа строка
+// marks - необязательное поле неизвестного типа
+
+interface SomeFile{
+  name: string,
+  type: TypesOfMedia,
+  format: FormatsOfMedia,
+  subtitles?: string,
+  marks?: unknown
+}
+
+function playMedia(
+    {name, type, format, subtitles, marks}: SomeFile = {
+      name: "example",
+      type: TypesOfMedia.audio,
+      format: FormatsOfMedia.mp4,
+    }
+): string {
+  let marksLog;
+
+  // Создать функционал, что если marks - это массив, то "сложить" все эелементы в одну строку и поместить в marksLog
+  // Если это строка, то просто поместить её в marksLog
+  // Если что-то другое - то marksLog = "Unsupported type of marks"
+  // Не допускайте any!
+
+  if (Array.isArray(marks)) {
+    marksLog = marks.join(' ');
+  } else if (typeof marks === 'string') {
+    marksLog = marks;
+  } else {
+    marksLog = "Unsupported type of marks"
+  }
+
+  console.log(`Media ${name}${format} is ${type}
+    Marks: ${marksLog}
+    Subtitles: ${subtitles ?? "none"}`);
+  // помните что это за оператор ??
+
+  return "Media started";
+}
+
+playMedia({
+  name: "WoW",
+  format: FormatsOfMedia.mp4,
+  type: TypesOfMedia.video,
+  subtitles: "hmhmhm hmhmhm doh",
+  marks: ["4:30", "5:40"],
+});

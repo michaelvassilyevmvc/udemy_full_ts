3a3802522d7e6ab916b0c2bddb76ed8cd140538a
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-11-12 15:11:57 +0500

    ch3_L39_end Перегрузка функций

--- ch3/test/index.ts
+++ ch3/test/index.ts
@@ -1,85 +1,31 @@
-function printMsg(msg: string[] | number | string): void {
-  if (Array.isArray(msg)) {
-    msg.forEach((m) => console.log(m));
-  } else if (isNumber(msg)) {
-    console.log(msg);
+interface Square {
+  side: number;
+  area: number;
+}
+
+interface Rect {
+  a: number;
+  b: number;
+  area: number;
+}
+
+function calculateArea(side: number): Square;
+function calculateArea(a: number, b: number):Rect;
+function calculateArea(a: number, b?: number): Square | Rect {
+  if (b) {
+    const rect: Rect = {
+      a, b,
+      area: a * b
+    }
+    return rect;
   } else {
-    console.log(msg);
+    const square: Square = {
+      side: a,
+      area: a * a,
+    }
+    return square;
   }
-  console.log(msg);
 }
 
-printMsg(4)
-
-// function isNumber(n: string[] | number | string): n is number {
-//   return typeof n === "number";
-// }
-
-function isNumber(n: unknown): n is number {
-  return typeof n === 'number';
-}
-
-interface Car {
-  name: 'car';
-  engine: string;
-  wheels: {
-    number: number;
-    type: string;
-  };
-}
-
-interface Ship {
-  name: 'ship';
-  engine: string;
-  sail: string;
-}
-
-interface Airplane {
-  name: 'airplane';
-  engine: string;
-  wings: string;
-}
-
-interface SuperAirplane {
-  name: "smth";
-  engine: string;
-  wings: string;
-}
-
-type  Vehicle = Car | Ship | Airplane | SuperAirplane;
-
-function repairVehicle(vehicle: Vehicle) {
-  // if (isCar(vehicle)) {
-  //   console.log(vehicle.wheels);
-  // } else if (isShip(vehicle)) {
-  //   console.log(vehicle.sail);
-  // } else {
-  //   const smth: never = vehicle;
-  // }
-  switch (vehicle.name) {
-    case 'car':
-      console.log(vehicle.wheels);
-      break;
-    case 'ship':
-      console.log(vehicle.sail);
-      break;
-    case 'airplane':
-      console.log(vehicle.wings);
-      break;
-    case "smth":
-      console.log(vehicle.wings);
-      break;
-    default:
-      const smth: never = vehicle;
-      console.log('Ouuuups!')
-  }
-
-}
-
-function isCar(car: Vehicle): car is Car {
-  return (car as Car).wheels.number !== undefined;
-}
-
-function isShip(ship: Vehicle): ship is Ship {
-  return "sail" in ship;
-}
+calculateArea(1);
+calculateArea(2, 5);

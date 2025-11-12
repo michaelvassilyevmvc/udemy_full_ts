e312ecb2ce17d356f62758eec37f92ce48c1e4c1
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-11-12 15:03:56 +0500

    ch3_L38_end Использования unknown и TypeGuard

--- ch3/test/index.ts
+++ ch3/test/index.ts
@@ -1,25 +1,85 @@
-const department: Department = {
-  name: 'web-dev',
-  budget: 50000
-}
-
-interface Department {
-  name: string,
-  budget: number
-}
-
-interface Project {
-  name: string,
-  projectBudget: number
-}
-
-
-
-function transformDepartment(department: Department, amount: number): Project {
-  return {
-    name: department.name,
-    projectBudget: amount
+function printMsg(msg: string[] | number | string): void {
+  if (Array.isArray(msg)) {
+    msg.forEach((m) => console.log(m));
+  } else if (isNumber(msg)) {
+    console.log(msg);
+  } else {
+    console.log(msg);
   }
+  console.log(msg);
 }
 
-const mainProject: Project = transformDepartment(department, 4000);
+printMsg(4)
+
+// function isNumber(n: string[] | number | string): n is number {
+//   return typeof n === "number";
+// }
+
+function isNumber(n: unknown): n is number {
+  return typeof n === 'number';
+}
+
+interface Car {
+  name: 'car';
+  engine: string;
+  wheels: {
+    number: number;
+    type: string;
+  };
+}
+
+interface Ship {
+  name: 'ship';
+  engine: string;
+  sail: string;
+}
+
+interface Airplane {
+  name: 'airplane';
+  engine: string;
+  wings: string;
+}
+
+interface SuperAirplane {
+  name: "smth";
+  engine: string;
+  wings: string;
+}
+
+type  Vehicle = Car | Ship | Airplane | SuperAirplane;
+
+function repairVehicle(vehicle: Vehicle) {
+  // if (isCar(vehicle)) {
+  //   console.log(vehicle.wheels);
+  // } else if (isShip(vehicle)) {
+  //   console.log(vehicle.sail);
+  // } else {
+  //   const smth: never = vehicle;
+  // }
+  switch (vehicle.name) {
+    case 'car':
+      console.log(vehicle.wheels);
+      break;
+    case 'ship':
+      console.log(vehicle.sail);
+      break;
+    case 'airplane':
+      console.log(vehicle.wings);
+      break;
+    case "smth":
+      console.log(vehicle.wings);
+      break;
+    default:
+      const smth: never = vehicle;
+      console.log('Ouuuups!')
+  }
+
+}
+
+function isCar(car: Vehicle): car is Car {
+  return (car as Car).wheels.number !== undefined;
+}
+
+function isShip(ship: Vehicle): ship is Ship {
+  return "sail" in ship;
+}

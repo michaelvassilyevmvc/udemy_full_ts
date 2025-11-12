ee65ff9b75f23b987a19ed754cfcb56813e7fa6c
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-11-12 15:20:31 +0500

    ch3_L40_end Работа с интерфейсами включающими множество других интерфейсов

--- ch3/test/index.ts
+++ ch3/test/index.ts
@@ -1,31 +1,68 @@
-interface Square {
-  side: number;
-  area: number;
+function isNumber(n: unknown): n is number {
+  return typeof n === 'number';
 }
 
-interface Rect {
-  a: number;
-  b: number;
-  area: number;
+interface Car {
+  name: 'car';
+  engine: string;
+  wheels: {
+    number: number;
+    type: string;
+  };
 }
 
-function calculateArea(side: number): Square;
-function calculateArea(a: number, b: number):Rect;
-function calculateArea(a: number, b?: number): Square | Rect {
-  if (b) {
-    const rect: Rect = {
-      a, b,
-      area: a * b
-    }
-    return rect;
-  } else {
-    const square: Square = {
-      side: a,
-      area: a * a,
-    }
-    return square;
+interface Ship {
+  name: 'ship';
+  engine: string;
+  sail: string;
+}
+
+interface Airplane {
+  name: 'airplane';
+  engine: string;
+  wings: string;
+}
+
+interface ComplexVehicle {
+  name: "car" | "ship" | "airplane";
+  engine: string;
+  wheels?: number;
+  sail?: string;
+  wings?: string;
+}
+
+type  Vehicle = Car | Ship | Airplane;
+
+const car: ComplexVehicle = {
+  name: 'car',
+  engine: 'V8'
+}
+
+function repairVehicle(vehicle: ComplexVehicle) {
+
+  switch (vehicle.name) {
+    case 'car':
+      console.log(vehicle.wheels);
+      break;
+    case 'ship':
+      console.log(vehicle.sail);
+      break;
+    case 'airplane':
+      console.log(vehicle.wings);
+      break;
+    default:
+      // const smth: never = vehicle;
+      console.log('Ouuuups!')
   }
+
 }
 
-calculateArea(1);
-calculateArea(2, 5);
+repairVehicle(car);
+
+function isCar(car: Vehicle): car is Car {
+  return (car as Car).wheels.number !== undefined;
+}
+
+function isShip(ship: Vehicle): ship is Ship {
+  return "sail" in ship;
+}

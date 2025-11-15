41b263118cdfe70a0d15387b40633a0f3aa8b1c1
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-11-15 19:57:08 +0500

    ch4_L50_end Наследование generic

--- ch4/index.ts
+++ ch4/index.ts
@@ -1,46 +1,28 @@
-interface ProcessingFn {
-  <T>(data: T): T;
-}
+class User<T, S> {
+  name: T;
+  age: S;
 
-function processing<T>(data: T): T {
-  return data;
-}
+  constructor(name: T, age: S) {
+    this.name = name;
+    this.age = age;
+  }
 
-let newFunc: ProcessingFn = processing;
-
-type Smth<T> = T;
-
-const num: Smth<number> = 5;
-
-interface ParentsOfUser {
-  mother: string,
-  father: string
-}
-
-type User<ParentsData extends ParentsOfUser> = {
-  login: string;
-  age: number;
-  parents: ParentsData;
-};
-
-const user: User<{ mother: string, father: string }> = {
-  login: 'str',
-  age: 54,
-  parents: {
-    mother: 'Anna',
-    father: 'no data',
+  sayMyFullName<T>(surname: T): string {
+    if (typeof surname !== 'string') {
+      return `I have only name: ${this.name}`;
+    } else {
+      return `${this.name} ${surname}`;
+    }
   }
 }
 
-type OrNull<Type> = Type | null;
-type OneOrMany<Type> = Type | Type[]
-
-const data: OneOrMany<number[]> = [5];
-
-const depositMoney = <T extends  number | string>(amount: T):T => {
-  console.log(`req to server with amount: ${amount}`);
-  return amount;
+class AdminUser<T> extends User<string, number> {
+  rules: T;
 }
 
-depositMoney(500);
-depositMoney("500");
+const ivan = new User("Ivan", 30);
+
+console.log(ivan);
+
+const alex = new User<string, number>('Alex', 30);
+
---
+++ ch4/self49.js
@@ -0,0 +1,93 @@
+// Создать Generic-интерфейс PlayerData, который подходил бы для создания таких объектов:
+var player1 = {
+    game: "CS:GO",
+    hours: 300,
+    server: "basic",
+};
+var player2 = {
+    game: 2048,
+    hours: "300 h.",
+    server: "arcade",
+};
+var player3 = {
+    game: "Chess",
+    hours: {
+        total: 500,
+        inMenu: 50,
+    },
+    server: "chess",
+};
+// Массив данных с фигурами содержит объекты, у каждого из которых обязательно есть свойство name
+// Каждый объект может еще содержать дополнительные свойства в случайном виде
+// Свойство name может иметь только 4 варианта
+// Функция calculateAmountOfFigures должна принимать массив с объектами, у которых обязательно должно быть свойство name
+// Возвращает она объект-экземпляр AmountOfFigures
+// Внутри себя подсчитывает сколько каких фигур было в массиве и записывает результаты в AmountOfFigures
+// С текущими данными в консоль должно попадать:
+// { squares: 3, circles: 2, triangles: 2, others: 1 }
+var FigureNames;
+(function (FigureNames) {
+    FigureNames["Rect"] = "rect";
+    FigureNames["Circle"] = "circle";
+    FigureNames["Triangle"] = "triangle";
+    FigureNames["Line"] = "line";
+})(FigureNames || (FigureNames = {}));
+function calculateAmountOfFigures(figure) {
+    var amount = {
+        squares: 0,
+        circles: 0,
+        triangles: 0,
+        others: 0,
+    };
+    figure.forEach(function (f) {
+        switch (f.name) {
+            case FigureNames.Rect:
+                amount.squares++;
+                break;
+            case FigureNames.Triangle:
+                amount.triangles++;
+                break;
+            case FigureNames.Circle:
+                amount.circles++;
+                break;
+            default:
+                amount.others++;
+                break;
+        }
+    });
+    return amount;
+}
+var data = [
+    {
+        name: FigureNames.Rect,
+        data: { a: 5, b: 10 },
+    },
+    {
+        name: FigureNames.Rect,
+        data: { a: 6, b: 11 },
+    },
+    {
+        name: FigureNames.Triangle,
+        data: { a: 5, b: 10, c: 14 },
+    },
+    {
+        name: FigureNames.Line,
+        data: { l: 15 },
+    },
+    {
+        name: FigureNames.Circle,
+        data: { r: 10 },
+    },
+    {
+        name: FigureNames.Circle,
+        data: { r: 5 },
+    },
+    {
+        name: FigureNames.Rect,
+        data: { a: 15, b: 7 },
+    },
+    {
+        name: FigureNames.Triangle,
+    },
+];
+console.log(calculateAmountOfFigures(data));
